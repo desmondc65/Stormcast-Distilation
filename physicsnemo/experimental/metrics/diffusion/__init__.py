28e9a30d4b94afb8c6a2b58d0901140b96e82171
dczy <dzycheong@gmail.com>
2026-05-14 03:57:29 +0800

    Refactor: remove unused consistency and DMD loss imports; add radial log PSD function

--- physicsnemo/experimental/metrics/diffusion/__init__.py
+++ physicsnemo/experimental/metrics/diffusion/__init__.py
@@ -14,7 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .consistency_loss import ConsistencyDistillationLoss
-from .dmd_loss import DMDLoss
 from .loss import tEDMResidualLoss
-from .progressive_distillation_loss import ProgressiveDistillationLoss
