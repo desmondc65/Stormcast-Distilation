488b1a8d47ed943e71fa0bfbc7f8a755a4233c50
dczy <dzycheong@gmail.com>
2026-06-12 23:32:44 +0800

    Increase total training steps for GS-MeanFlow to enhance model performance

--- stormcast/zettabyte_scripts/train_gsmeanflow.sh
+++ stormcast/zettabyte_scripts/train_gsmeanflow.sh
@@ -63,7 +63,7 @@ weight_decay=1.0E-4
 adam_betas="[0.9,0.999]"
 lr_warmup_steps=4000
 min_lr_ratio=0.01          # min_lr = lr * min_lr_ratio
-total_train_steps=40000
+total_train_steps=100000
 clip_grad_norm=1.0
 loss='gsmeanflow'
 fp_optimizations='fp32'
